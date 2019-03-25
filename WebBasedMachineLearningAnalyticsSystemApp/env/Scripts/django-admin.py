7a7aa335e327aedc6d608b70e0476f8e9d6933d4
Eric Brichetto <Eric.Brichetto@toro.com>
2019-03-25 16:33:32 -0500

    Add project files.

---
+++ WebBasedMachineLearningAnalyticsSystemApp/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\brichem\source\repos\WebBasedMachineLearningAnalyticsSystemApp\WebBasedMachineLearningAnalyticsSystemApp\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
